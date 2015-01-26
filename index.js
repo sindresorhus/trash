089d58731d89d312a795a388ee03e1f57816c1e0
Sindre Sorhus <sindresorhus@gmail.com>
2015-01-26 16:04:26 +0800

    move the Windows handling into a separate module
    https://github.com/sindresorhus/win-trash

--- index.js
+++ index.js
@@ -1,37 +1,14 @@
 'use strict';
-var path = require('path');
-var execFile = require('child_process').execFile;
-var osxTrash = require('osx-trash');
-var xdgTrash = require('xdg-trash');
-
-function win(paths, cb) {
-	execFile('./Recycle.exe', ['-f'].concat(paths), {
-		cwd: path.join(__dirname, 'vendor', 'cmdutils')
-	}, function (err) {
-		cb(err);
-	});
-}
-
 module.exports = function (paths, cb) {
-	if (!Array.isArray(paths)) {
-		throw new Error('`paths` required');
-	}
-
-	cb = cb || function () {};
-
-	paths = paths.map(function (el) {
-		return path.resolve(String(el));
-	});
-
 	if (process.platform === 'darwin') {
-		osxTrash(paths, cb);
+		require('osx-trash')(paths, cb);
 		return;
 	}
 
 	if (process.platform === 'win32') {
-		win(paths, cb);
+		require('win-trash')(paths, cb);
 		return;
 	}
 
-	xdgTrash(paths, cb);
+	require('xdg-trash')(paths, cb);
 };
