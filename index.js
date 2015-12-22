96d7c7ae5f6e69eff99720b0e4ad96c00d1aeca3
Mick van Gelderen <mickvangelderen@gmail.com>
2015-12-22 12:47:03 +0100

    Simplified the rest of the mapping and filtering.

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ var path = require('path');
 var pathExists = require('path-exists');
 var globby = require('globby');
 
+// A resolve that does not accept variadic arguments so it can be used with map.
+function resolvePath(x) {
+	return path.resolve(x);
+}
+
 module.exports = function (paths) {
 	if (!Array.isArray(paths)) {
 		return Promise.reject(new TypeError('Expected an array'));
 	}
 
-	paths = paths.map(String);
-
-	paths = globby.sync(paths, {nonull: true});
-
-	paths = paths.map(function (x) {
-		return path.resolve(x);
-	}).filter(function (x) {
-		return pathExists.sync(x);
-	});
+	paths = globby.sync(paths.map(String), {nonull: true})
+		.map(resolvePath)
+		.filter(pathExists.sync);
 
 	if (paths.length === 0) {
 		return Promise.resolve();
