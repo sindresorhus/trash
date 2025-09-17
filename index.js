ee611b9093cfb7342312d7f1604c19b2a0c3b14c
Lucas Garron <code@garron.net>
2025-09-17 03:45:34 -0700

    Use static string path imports to load the platform-specific implementations.
    This allows the imports to be statically analyzed and bundled by the widest variety of tooling.
    
    Closes: https://github.com/sindresorhus/trash/issues/130

--- index.js
+++ index.js
@@ -61,11 +61,22 @@ export default async function trash(paths, options = {}) {
 		return;
 	}
 
-	// Load platform-specific implementation
-	const platform = process.platform === 'darwin'
-		? 'macos'
-		: (process.platform === 'win32' ? 'windows' : 'linux');
-
-	const {default: trashFunction} = await import(`./lib/${platform}.js`);
+	const {default: trashFunction} = await platformSpecificImplementation();
 	return trashFunction(resolvedPaths);
+}
+
+async function platformSpecificImplementation() {
+	switch (process.platform) {
+		case 'darwin': {
+			return import('./lib/macos.js');
+		}
+
+		case 'win32': {
+			return import('./lib/windows.js');
+		}
+
+		default: {
+			return import('./lib/linux.js');
+		}
+	}
 }
